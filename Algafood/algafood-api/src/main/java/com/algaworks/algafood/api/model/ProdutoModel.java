c9cf88c5d93e0a2c68a8370351cd95f35176d69e
Leonardo Costa <leonardoeverton.tec@gmail.com>
2020-11-11 15:15:20 -0300

    12.25. Usando IDs vs UUIDs nas URIs de recursos

--- Algafood/algafood-api/src/main/java/com/algaworks/algafood/api/model/ProdutoModel.java
+++ Algafood/algafood-api/src/main/java/com/algaworks/algafood/api/model/ProdutoModel.java
@@ -2,20 +2,17 @@ package com.algaworks.algafood.api.model;
 
 import java.math.BigDecimal;
 
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-public class RestauranteModel {
+public class ProdutoModel {
 
 	private Long id;
 	private String nome;
-	private BigDecimal taxaFrete;
-	private CozinhaModel cozinha;
+	private String descricao;
+	private BigDecimal preco;
 	private Boolean ativo;
-	private Boolean aberto;
-	private EnderecoModel endereco;
 	
 }
--- Algafood/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/UsuarioRepository.java
+++ Algafood/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/UsuarioRepository.java
@@ -9,7 +9,6 @@ import com.algaworks.algafood.domain.model.Usuario;
 @Repository
 public interface UsuarioRepository extends CustomJpaRepository<Usuario, Long> {
 
-
 	Optional<Usuario> findByEmail(String email);
 	
 }
