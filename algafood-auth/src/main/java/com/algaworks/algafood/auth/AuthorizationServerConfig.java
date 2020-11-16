f7ac2e1d15bfad0de8f4b049e729dad379c559ab
Leonardo Costa <leonardoeverton.tec@gmail.com>
2020-11-16 13:45:38 -0300

    22.19. Testando o fluxo Authorization Code com um client JavaScript

--- algafood-auth/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -36,6 +36,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				.refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
 			
 			.and()
+				.withClient("foodanalytics")
+				.secret(passwordEncoder.encode("food123"))
+				.authorizedGrantTypes("authorization_code")
+				.scopes("write", "read")
+				.redirectUris("http://localhost:8082")
+				
+			.and()
 				.withClient("faturamento")
 				.secret(passwordEncoder.encode("faturamento123"))
 				.authorizedGrantTypes("client_credentials")
