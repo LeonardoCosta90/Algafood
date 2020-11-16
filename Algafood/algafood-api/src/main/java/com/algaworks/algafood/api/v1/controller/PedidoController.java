acf085b95e0ee9b0f1d60430bc7e170ac8f21a5c
Leonardo Costa <leonardoeverton.tec@gmail.com>
2020-11-16 17:45:44 -0300

    23.19. Carregando as permissões concedidas na autenticação

--- Algafood/algafood-api/src/main/java/com/algaworks/algafood/api/v1/controller/PedidoController.java
+++ Algafood/algafood-api/src/main/java/com/algaworks/algafood/api/v1/controller/PedidoController.java
@@ -29,6 +29,7 @@ import com.algaworks.algafood.api.v1.model.input.PedidoInput;
 import com.algaworks.algafood.api.v1.openapi.controller.PedidoControllerOpenApi;
 import com.algaworks.algafood.core.data.PageWrapper;
 import com.algaworks.algafood.core.data.PageableTranslator;
+import com.algaworks.algafood.core.security.AlgaSecurity;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.filter.PedidoFilter;
@@ -60,6 +61,9 @@ public class PedidoController implements PedidoControllerOpenApi {
 	@Autowired
 	private PagedResourcesAssembler<Pedido> pagedResourcesAssembler;
 	
+	@Autowired
+	private AlgaSecurity algaSecurity;
+	
 	@Override
 	@GetMapping
 	public PagedModel<PedidoResumoModel> pesquisar(PedidoFilter filtro, 
@@ -81,9 +85,8 @@ public class PedidoController implements PedidoControllerOpenApi {
 		try {
 			Pedido novoPedido = pedidoInputDisassembler.toDomainObject(pedidoInput);
 
-			// TODO pegar usuário autenticado
 			novoPedido.setCliente(new Usuario());
-			novoPedido.getCliente().setId(1L);
+			novoPedido.getCliente().setId(algaSecurity.getUsuarioId());
 
 			novoPedido = emissaoPedido.emitir(novoPedido);
 
--- algafood-auth/src/main/java/com/algaworks/algafood/auth/domain/Usuario.java
+++ algafood-auth/src/main/java/com/algaworks/algafood/auth/domain/Usuario.java
@@ -1,6 +1,5 @@
-package com.algaworks.algafood.domain.model;
+package com.algaworks.algafood.auth.domain;
 
-import java.time.OffsetDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,8 +11,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-
-import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -37,25 +34,9 @@ public class Usuario {
 	@Column(nullable = false)
 	private String senha;
 	
-	@CreationTimestamp
-	@Column(nullable = false, columnDefinition = "datetime")
-	private OffsetDateTime dataCadastro;
-	
 	@ManyToMany
 	@JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name = "usuario_id"),
 			inverseJoinColumns = @JoinColumn(name = "grupo_id"))
 	private Set<Grupo> grupos = new HashSet<>();
-	
-	public boolean removerGrupo(Grupo grupo) {
-		return getGrupos().remove(grupo);
-	}
-	
-	public boolean adicionarGrupo(Grupo grupo) {
-		return getGrupos().add(grupo);
-	}
-	
-	public boolean isNovo() {
-		return getId() == null;
-	}
 	
 }
